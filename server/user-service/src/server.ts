466599b306ea7b02df270675343410a46c581300
Vusal Huseynov <vslh333222111@gmail.com>
2025-08-07 18:21:10 +0400

    refactor(user-service): remove Express server setup and related middleware from user service

--- server/user-service/src/server.ts
+++ server/user-service/src/server.ts
@@ -1,34 +1,11 @@
-import express from 'express';
 import mongoose from 'mongoose';
-// import { Server } from 'http';
 import * as grpc from '@grpc/grpc-js';
 
 import { config } from '@/config';
 import { connectDatabase } from '@/db/connection';
 import logger from '@/lib/logger';
-import router from '@/routes';
-import { errorHandler } from '@/middlewares/error-handler.middleware';
 import { UserServiceService } from './generated/user_service';
 import { userServiceImplementation } from './grpc/user-service-impl';
-
-// let server: Server;
-// const PORT = process.env.PORT || 3000;
-
-const app = express();
-
-app.get('/health', (_req, res) => {
-  res.status(200).json({ status: 'ok' });
-});
-
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Router
-app.use('/api/users', router);
-
-// Error handler
-app.use(errorHandler);
 
 const grpcServer = new grpc.Server();
 
@@ -61,10 +38,6 @@ async function initialize() {
   await connectDatabase(`${config.MONGO_URI}`);
 
   setupGrpcServer();
-
-  // server = app.listen(PORT, () => {
-  //   logger.info(`User service is running on PORT:${PORT}`);
-  // });
 }
 
 initialize();
@@ -77,10 +50,9 @@ async function shutdown() {
 
   grpcServer.forceShutdown();
 
-  // server?.close(() => {
-  //   logger.warn('User service has shut down gracefully.');
-  //   process.exit(0);
-  // });
+  logger.info('User service has been shut down gracefully.');
+
+  process.exit(0);
 }
 
 process.on('SIGINT', shutdown);
